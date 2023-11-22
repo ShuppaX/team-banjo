4c1e614514177331c44ce869ad8a19d7197f7562
Cats <tomi.sepanmaa@gmail.com>
2023-11-22 19:29:19 +0200

    Fixed button position when tween spammed. Added Tween_Punch to Button.prefab

--- Assets/+TeamBanjo/Scripts/UI/Tween_Punch.cs
+++ Assets/+TeamBanjo/Scripts/UI/Tween_Punch.cs
@@ -12,19 +12,12 @@ namespace TeamBanjo.Tween
         [SerializeField] private int vibrato = 10;
         [SerializeField, Range(0, 1)] private float elasticity = 1;
         [SerializeField] private bool snapping = true;
-        [SerializeField] private Vector3 originalPosition;
-
-        private void Start()
-        {
-            Debug.Log($"Original position: {transform.localPosition}");
-            originalPosition = transform.localPosition;
-        }
 
         public void OnTweenPunch()
         {
             // Ensure that the object is in the original position before tweening it.
             // Otherwise the object might shift if tween is spammed.
-            transform.localPosition = Vector3.zero;
+            transform.DOComplete();
 
             transform.DOPunchPosition(punch, duration, vibrato, elasticity, snapping);
         }
